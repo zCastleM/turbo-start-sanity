f5bcad1460781beafb7f19a35b291b5453360e1f
hrithikroboto <hrithik@robotostudio.com>
2025-01-08 17:37:05 +0530

    added typegen client for sanity

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,10 +10,13 @@ async function fetchHomePageData() {
 }
 
 export default async function Page() {
+  const homePageData = await fetchHomePageData();
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">
+          {homePageData.data?.title ?? "Hello World"}
+        </h1>
         <Button size="sm">Button</Button>
         <Input placeholder="Search" />
       </div>
