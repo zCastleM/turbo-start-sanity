1d5df615691ccb89f13b30e71701a5ced7acf54c
hrithikroboto <hrithik@robotostudio.com>
2025-01-09 12:10:52 +0530

    feat: Add resolve logic for presentation plugin

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -5,7 +5,8 @@ import {schemaTypes} from './schemaTypes'
 import { structure } from "./structure";
 import { iconPicker } from "sanity-plugin-icon-picker";
 import { presentationUrl } from "./plugins/presentation-url";
-
+import { presentationTool } from "sanity/presentation";
+import { resolve } from "./resolve-presentation-document";
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID ?? "";
 const dataset = process.env.SANITY_STUDIO_DATASET ?? "production";
@@ -22,8 +23,27 @@ export default defineConfig({
     }),
     visionTool(),
     iconPicker(),
+    presentationTool({
+      resolve: resolve,
+      previewUrl: {
+        origin:
+          window.location.hostname === "localhost"
+            ? "http://localhost:3000"
+            : "https://template.roboto.studio",
+        previewMode: {
+          enable: "/api/presentation-draft",
+        },
+      },
+    }),
     presentationUrl(),
   ],
+  document: {
+    newDocumentOptions: (prev, { creationContext }) => {
+      const { type } = creationContext;
+      if (type === "global") return [];
+      return prev;
+    },
+  },
   schema: {
     types: schemaTypes,
   },
