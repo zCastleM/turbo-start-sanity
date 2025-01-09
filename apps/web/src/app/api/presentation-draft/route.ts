2bc19c0a2d07ce1b2df3053c9e21eac1f423d8c1
hrithikroboto <hrithik@robotostudio.com>
2025-01-09 12:14:24 +0530

    feat: Add SANITY_API_TOKEN to globalEnv in turbo.json

---
+++ apps/web/src/app/api/presentation-draft/route.ts
@@ -0,0 +1,38 @@
+import { validatePreviewUrl } from "@sanity/preview-url-secret";
+import { draftMode } from "next/headers";
+import { type NextRequest, NextResponse } from "next/server";
+
+import { client } from "@/lib/sanity/client";
+
+const clientWithToken = client.withConfig({
+  token: process.env.SANITY_API_TOKEN,
+});
+
+export async function GET(request: NextRequest) {
+  if (!process.env.SANITY_API_TOKEN) {
+    return new Response(
+      "Missing environment variable SANITY_API_TOKEN",
+      {
+        status: 500,
+      }
+    );
+  }
+
+  const { isValid } = await validatePreviewUrl(
+    clientWithToken,
+    request.url
+  );
+
+  const params = new URLSearchParams(request.nextUrl.searchParams);
+  const preview = params.get("sanity-preview-pathname");
+
+  if (!isValid) {
+    return new Response("Invalid secret", { status: 401 });
+  }
+
+  (await draftMode()).enable();
+  if (preview) {
+    return NextResponse.redirect(new URL(preview, request.url));
+  }
+  return NextResponse.redirect(new URL("/", request.url));
+}
