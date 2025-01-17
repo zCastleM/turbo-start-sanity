3f7d671ccde3476f3ee63d63f4f334a128bc5320
hrithikroboto <hrithik@robotostudio.com>
2025-01-17 16:08:07 +0530

    style: Update deployment workflow and set HOST_NAME variable

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,6 +2,7 @@ import { defineCliConfig } from "sanity/cli";
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
 const dataset = process.env.SANITY_STUDIO_DATASET;
+const host = process.env.HOST_NAME;
 // const title = process.env.SANITY_STUDIO_TITLE;
 // const presentationUrl = process.env.SANITY_STUDIO_PRESENTATION_URL;
 
@@ -10,6 +11,7 @@ export default defineCliConfig({
     projectId: projectId,
     dataset: dataset,
   },
+  studioHost: host ? `${host}-template-roboto` : "template-roboto",
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
