08093bdedc493aa8502ded8eac5554bc92ef2943
hrithikroboto <hrithik@robotostudio.com>
2025-01-28 17:23:02 +0530

    style: Improve structure and alignment in HeroBlock

--- apps/web/src/components/sections/hero.tsx
+++ apps/web/src/components/sections/hero.tsx
@@ -19,17 +19,23 @@ export function HeroBlock({
     <section className="">
       <div className="container mx-auto px-4 md:px-6">
         <div className="grid items-center gap-8 lg:grid-cols-2">
-          <div className="flex flex-col gap-4 items-center justify-center text-center lg:items-start lg:text-left">
+          <div className="grid h-full grid-rows-[auto_1fr_auto] gap-4 items-center justify-items-center text-center lg:items-start lg:justify-items-start lg:text-left">
             <Badge variant="outline">{badge}</Badge>
-            <h1 className="text-pretty text-4xl font-bold lg:text-6xl">
-              {title}
-            </h1>
-            <RichText richText={richText} />
-            <SanityButtons
-              buttons={buttons}
-              buttonClassName="w-full sm:w-auto"
-              className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start"
-            />
+            <div className="grid gap-4">
+              <h1 className="text-pretty text-4xl font-bold lg:text-6xl">
+                {title}
+              </h1>
+              <div className="text-base text-balance">
+                <RichText richText={richText} />
+              </div>
+            </div>
+            <div>
+              <SanityButtons
+                buttons={buttons}
+                buttonClassName="w-full sm:w-auto"
+                className="grid w-full gap-2 sm:grid-flow-col lg:justify-start"
+              />
+            </div>
           </div>
           {image && (
             <div className="h-96 w-full">
