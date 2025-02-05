adae6edf6e20a8ebbedeb4f155dd4a12d5878476
Jonathan Alford <jono@robotostudio.com>
2025-02-05 13:05:46 +0530

    Update CTACard and SanityImage components for better image display

--- apps/web/src/components/cta-card.tsx
+++ apps/web/src/components/cta-card.tsx
@@ -16,22 +16,27 @@ export function CTACard({ card, className }: CTACardProps) {
     <Link
       href={url ?? "#"}
       className={cn(
-        "rounded-3xl p-8 md:p-10 transition-colors bg-accent hover:bg-accent/60",
+        "rounded-3xl p-4 md:p-8 transition-colors relative overflow-hidden group flex flex-col justify-end lg:h-[400px]",
         className,
       )}
     >
-      <div className="flex flex-col space-y-2">
-        {image?.asset && (
+      {image?.asset && (
+        <div className="absolute inset-0 z-[1] mix-blend-multiply">
           <SanityImage
             asset={image}
             loading="eager"
             priority
             quality={100}
-            className="mb-6"
+            fill
+            className="object-cover grayscale pointer-events-none group-hover:opacity-100 group-hover:transition-opacity duration-1000 opacity-40"
           />
-        )}
+        </div>
+      )}
+      <div className="z-[2] pt-64 flex flex-col space-y-2 mb-4 translate-y-0 lg:translate-y-16 duration-500 lg:group-hover:translate-y-0 lg:absolute lg:top-8 lg:inset-x-8">
         <h3 className="text-xl font-[500] text-[#111827]">{title}</h3>
-        <p className="text-sm text-[#374151]">{description}</p>
+        <p className="text-sm text-[#374151] lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300 delay-150">
+          {description}
+        </p>
       </div>
     </Link>
   );
