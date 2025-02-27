32058e356fcd1e581288c8f75dc0a2851b94634c
aayushroboto <aayush@robotostudio.com>
2025-02-27 12:45:11 +0530

    feat: Add dark mode styles to Footer and Image Link Card components

--- apps/web/src/components/image-link-card.tsx
+++ apps/web/src/components/image-link-card.tsx
@@ -28,13 +28,13 @@ export function CTACard({ card, className }: CTACardProps) {
             priority
             quality={100}
             fill
-            className="object-cover grayscale pointer-events-none group-hover:opacity-100 group-hover:transition-opacity duration-1000 opacity-40"
+            className="object-cover grayscale pointer-events-none group-hover:opacity-100 group-hover:transition-opacity duration-1000 opacity-40 dark:opacity-80"
           />
         </div>
       )}
       <div className="z-[2] pt-64 flex flex-col space-y-2 mb-4 duration-500 xl:absolute xl:top-24 group-hover:top-8 xl:inset-x-8">
-        <h3 className="text-xl font-[500] text-[#111827]">{title}</h3>
-        <p className="text-sm text-[#374151] xl:opacity-0 xl:group-hover:opacity-100 transition-opacity duration-300 delay-150">
+        <h3 className="text-xl font-[500] text-[#111827] dark:text-neutral-300">{title}</h3>
+        <p className="text-sm text-[#374151] xl:opacity-0 xl:group-hover:opacity-100 transition-opacity duration-300 delay-150 dark:text-neutral-300">
           {description}
         </p>
       </div>
